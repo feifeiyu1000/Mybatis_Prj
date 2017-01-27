49874e0d69caec4713b966efdb0653e08da60ad1
marouaneunix <elmerrouni.marouane@gmail.com>
2017-01-27 22:36:44 +0000

    config resources and mybatisConfig

--- src/main/java/com/springHello/controller/HomeController.java
+++ src/main/java/com/springHello/controller/HomeController.java
@@ -1,14 +1,23 @@
 package com.springHello.controller;
 
+import com.springHello.bean.Book;
+import com.springHello.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 /**
  * Created by root on 1/22/17.
  */
 @Controller
 public class HomeController {
+
+    @Autowired
+    private BookService bookService;
 
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public String getHome(){
@@ -18,6 +27,13 @@ public class HomeController {
     @RequestMapping(path = "/home", method = RequestMethod.GET)
     public String getHome1(){
         return "index";
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+        List<Book> books = bookService.findAllBooks();
+        model.addAttribute("books",books);
+        return "books";
     }
 
 
