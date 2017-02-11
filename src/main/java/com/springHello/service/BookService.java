92c7cc81997dc36a4d06dc1b984417f5abda3f0b
marouaneunix <elmerrouni.marouane@gmail.com>
2017-02-11 01:18:16 +0000

    config @PreAuthorize spring Security OK

--- src/main/java/com/springHello/service/BookService.java
+++ src/main/java/com/springHello/service/BookService.java
@@ -1,6 +1,7 @@
 package com.springHello.service;
 
 import com.springHello.bean.Book;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
 
@@ -11,6 +12,7 @@ public interface BookService {
     Book findBookById(Long id);
     List<Book> findAllBooks();
     void insertBook(Book book);
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     void deleteBook(Long id);
     Book findBookByName(String name);
 }
