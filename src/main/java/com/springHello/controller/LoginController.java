10f594aa8759fe7e1b4c0c96362d766fc20aafe2
marouaneunix <elmerrouni.marouane@gmail.com>
2017-02-11 00:46:54 +0000

    logout configuration VERY OK

--- src/main/java/com/springHello/controller/LoginController.java
+++ src/main/java/com/springHello/controller/LoginController.java
@@ -6,6 +6,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * Created by mar1 on 2/10/17.
  */
@@ -25,4 +28,15 @@ public class LoginController {
     public String RootForm(){
         return "redirect:/";
     }
+    /*
+    @GetMapping("/logout")
+    public String getLogout(HttpServletResponse response){
+        Cookie cookie = new Cookie("JSESSIONID", null); // Not necessary, but saves bandwidth.
+        cookie.setHttpOnly(true);
+        cookie.setMaxAge(0); // Don't set to -1 or it will become a session cookie!
+        response.addCookie(cookie);
+        return "redirect:/";
+    }
+    */
+
 }
